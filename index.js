58d94afea87c0d554336a3fb3b7398bbdaa1cb0b
theresamorelli <theresa.l.morelli@gmail.com>
2019-03-30 16:34:51 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class Main extends React.Component {
+  render() {
+    return (<div>Get a website in 15 minutes or less</div>)
+    
+  }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(<Main/>, domContainer);
