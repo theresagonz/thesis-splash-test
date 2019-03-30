803a991f7e89aa5a1463e6152fb7a9e02cf0ffcd
theresamorelli <theresa.l.morelli@gmail.com>
2019-03-30 17:05:01 -0400

    Try to add React

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict';
+import NavBar from './src/components/navbar.js';
+// import Welcome from './src/components/welcome';
+// import OurServices from './src/components/our-services';
+// import Footer from './src/components/footer';
 
 class Main extends React.Component {
   render() {
-    return (<div>Get a website in 15 minutes or less</div>)
-    
+    console.log('in main')
+    return (
+      <div>
+        <NavBar />
+        {/* <Welcome />
+        <OurServices />
+        <Footer /> */}
+      </div>
+    );
   }
 }
 
 const domContainer = document.querySelector('#root');
-ReactDOM.render(<Main/>, domContainer);
+ReactDOM.render(<Main />, domContainer);
